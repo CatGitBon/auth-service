05c0864891131fdd7d1d1787507576d67ddf5b5e
ozm <ozm@debian.ru>
2025-07-12 12:47:45 +0500

    config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,12 +1,40 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"fmt"
+	"strings"
 
-func LoadConfig(path string) (Config, error) {
+	"github.com/spf13/viper"
+)
+
+type DatabaseConfig struct {
+	Host     string `mapstructure:"host"`
+	Port     string `mapstructure:"port"`
+	User     string `mapstructure:"user"`
+	Password string `mapstructure:"password"`
+	Name     string `mapstructure:"name"`
+}
+
+type AppConfig struct {
+	Database DatabaseConfig `mapstructure:"database"`
+}
+
+func LoadConfig(path string) (AppConfig, error) {
+	var config AppConfig
+
 	viper.SetConfigFile(path)
-	viper.SetConfigType("yaml")
 
+	// Включаем поддержку переменных окружения
 	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
-	return Config{}, nil
+	if err := viper.ReadInConfig(); err != nil {
+		return config, fmt.Errorf("error reading config file: %w", err)
+	}
+
+	if err := viper.Unmarshal(&config); err != nil {
+		return config, fmt.Errorf("unable to unmarshal config: %w", err)
+	}
+
+	return config, nil
 }
